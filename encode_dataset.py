920ac73f609a329b23125c8a0b62a44a1873083e
Tamanna Tahseen Hossain <tthossai@ava-s5.ics.uci.edu>
2024-07-12 16:39:48 -0700

    Update encode

--- encode_dataset.py
+++ encode_dataset.py
@@ -8,7 +8,7 @@ import pickle
 def get_hidden_states(text, model, tokenizer):
     inputs = tokenizer(text, return_tensors="pt").to("cuda")
     outputs = model(inputs["input_ids"], output_hidden_states = True)
-    hidden_states = outputs.cache_params  # Take the last hidden state
+    hidden_states = outputs.cache_params  
     return hidden_states
 
 def read_dataset(filename):
@@ -53,3 +53,32 @@ encoded_dataset = encode_dataset(dataset)
 # Save encoded_dataset as pickle file
 with open("encoded_training_subset.pkl", "wb") as f:
     pickle.dump(encoded_dataset, f)
+
+#########################
+##### Batch Encode ######
+#########################
+'''
+from accelerate import Accelerator, DistributedType
+
+accelerator = Accelerator()
+text_column_name = 'text'
+'''
+
+block_size=model.config.max_position_embeddings
+
+# Main data processing function that will concatenate all texts from our dataset and generate chunks of block_size.
+def group_texts(examples):
+    # Concatenate all texts.
+    concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the small remainder, and if the total_length < block_size  we exclude this batch and return an empty dict.
+    # We could add padding if the model supported it instead of this drop, you can customize this part to your needs.
+    total_length = (total_length // block_size) * block_size
+    # Split by chunks of max_len.
+    result = {
+        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+        for k, t in concatenated_examples.items()
+    }
+    return result
+
+
