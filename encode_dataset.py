11ff5a3538d4da33ce860cd562e7fff8546ebc22
Tamanna Hossain-Kay <34294551+atleastbeth3@users.noreply.github.com>
2024-07-11 19:54:09 -0700

    Updated encoding and training

--- encode_dataset.py
+++ encode_dataset.py
@@ -1,11 +1,14 @@
 from transformers import MambaConfig, MambaForCausalLM, AutoTokenizer
 import torch
 import json
+from torch.utils.data import DataLoader
+from datasets import Dataset
+import pickle
 
 def get_hidden_states(text, model, tokenizer):
     inputs = tokenizer(text, return_tensors="pt").to("cuda")
     outputs = model(inputs["input_ids"], output_hidden_states = True)
-    hidden_states = outputs.hidden_states[-1]  # Take the last hidden state
+    hidden_states = outputs.cache_params  # Take the last hidden state
     return hidden_states
 
 def read_dataset(filename):
@@ -17,15 +20,27 @@ def read_dataset(filename):
     return texts
 
 def encode_dataset(dataset):
-    encoded_dataset=[]
+    hidden_states=[]
+    labels=[]
     for idx, text in enumerate(dataset):
         print(idx)
         hidden_state= get_hidden_states(text[:50], model, tokenizer)
-        encoded_dataset.append({
-                                 "input_embeds": hidden_state.tolist(),
-                                 "labels": text[:50]
-                                })
+        hidden_states.append(hidden_state)
+        labels.append(text[:50])
+    encoded_dataset = {'cache_params': hidden_states, 'labels': labels}
     return encoded_dataset
+
+class EncodedDataset(Dataset):
+    def __init__(self, encoded_dataset):
+        self.encodings = encoded_dataset["cache_params"]
+        self.labels = encoded_dataset["labels"]
+    def __getitem__(self, idx):
+        item = {}
+        item["cache_params"] = self.encodings[idx]
+        item["labels"] = self.labels[idx]
+        return item
+    def __len__(self):
+        return len(self.labels)
 
 model_id = "state-spaces/mamba-130m-hf"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
@@ -35,5 +50,6 @@ model.cuda()
 dataset = read_dataset("train_subset.jsonl")
 encoded_dataset = encode_dataset(dataset)
 
-with open('encoded_train_subset.json', 'w') as f:
-    json.dump(encoded_dataset, f, indent=4)
+# Save encoded_dataset as pickle file
+with open("encoded_training_subset.pkl", "wb") as f:
+    pickle.dump(encoded_dataset, f)
--- training.py
+++ training.py
@@ -1,79 +1,38 @@
 from transformers import MambaConfig, MambaForCausalLM, AutoTokenizer
 import torch
-import pandas as pd
-import json
-from torch.utils.data import Dataset
 
-###############
-### Dataset ###
-###############
 tokenizer = AutoTokenizer.from_pretrained("state-spaces/mamba-130m-hf")
-mamba = MambaForCausalLM.from_pretrained("state-spaces/mamba-130m-hf")
+model = MambaForCausalLM.from_pretrained("state-spaces/mamba-130m-hf")
+model.cuda()
 
-def get_hidden_states(text, model, tokenizer):
-    inputs = tokenizer(text, return_tensors="pt")
-    outputs = model(**inputs)
-    hidden_states = outputs.hidden_states[-1]  # Take the last hidden state
-    return hidden_states
+#####################
+### Hidden States ###
+#####################
+text = "Hello, this is a test"
 
-class EncodingsDataset(Dataset):
-    def __init__(self, encodings, labels):
-        self.encodings = encodings
-        self.labels = labels
+inputs = tokenizer(text, return_tensors="pt").to("cuda")
 
-    def __getitem__(self, idx):
-        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-        item["labels"] = torch.tensor(self.labels[idx])
-        return item
+outputs = model(inputs["input_ids"], 
+                output_hidden_states = True , 
+                use_cache = True, 
+                return_dict=True)
+cache = outputs.cache_params
 
-    def __len__(self):
-        return len(self.labels)
+#############
+### Train ###
+#############
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+optimizer.zero_grad()
 
-file_path = "train_subset.jsonl"
-texts = []
+inputs = tokenizer(tokenizer.bos_token, return_tensors="pt").to("cuda")
+labels = tokenizer("Label", return_tensors="pt").to("cuda")
 
-with open(file_path, "r") as f:
-    for line in f:
-        data = json.loads(line)
-        texts.append(data["text"])
-
-hidden_states = [get_hidden_states(text, mamba, tokenizer) for text in texts]
-
-encodings = {"input_ids": hidden_states}
-dataset = EncodingsDataset(encodings, texts)
-
-################
-### Training ###
-################
-from trl import SFTTrainer
-from peft import LoraConfig
-from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments
-
-model_id = "state-spaces/mamba-130m-hf"
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(model_id)
-
-training_args = TrainingArguments(
-    output_dir="./results",
-    num_train_epochs=3,
-    per_device_train_batch_size=4,
-    logging_dir='./logs',
-    logging_steps=10,
-    learning_rate=2e-3
-)
-
-lora_config =  LoraConfig(
-        r=8,
-        target_modules=["x_proj", "embeddings", "in_proj", "out_proj"],
-        task_type="CAUSAL_LM",
-        bias="none"
-)
-
-trainer = SFTTrainer(
-    model=model,
-    args=training_args,
-    peft_config=lora_config,
-    train_dataset=dataset,
-    #dataset_text_field="quote",
-)
-trainer.train()
+model.train()
+outputs = model(input_ids=inputs["input_ids"], 
+                cache_params = cache, 
+                use_cache=True, 
+                labels=labels["input_ids"])
+loss = outputs.loss
+loss.backward()
+optimizer.step()
