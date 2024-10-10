27527d8c2a0e4cfef8e2af8b18e5c668f595f81c
Ganesh Jagadeesan <cjagadeesan@dataminr.com>
2024-10-10 14:52:47 +0000

    +fix: updated data

--- src/ouroboros/utils/data.py
+++ src/ouroboros/utils/data.py
@@ -1,10 +1,14 @@
+from collections.abc import Mapping
+from dataclasses import dataclass, field
 from typing import Any, Dict, List, Optional, Tuple, Union
 
 import torch
-from datasets import Dataset, Features, load_dataset
+from datasets import Dataset, Features, load_dataset, IterableDataset
 from torch import Generator
 from torch.nn.functional import pad
 from transformers import PreTrainedTokenizerBase
+from transformers.utils.generic import PaddingStrategy
+from transformers.data.data_collator import DataCollatorForLanguageModeling, pad_without_fast_tokenizer_warning, _torch_collate_batch
 
 
 random_generator = Generator()
@@ -14,7 +18,8 @@ random_generator = random_generator.manual_seed(2147483647)
 def load_dataset_from_files_or_hf(
         type_or_huggingface_path: str = "json",
         filepaths: Optional[Union[str, List[str], Dict[str, Union[str, List[str]]]]] = None, 
-        field: Optional[str] = None, split: Optional[str] = None,
+        split: Optional[str] = None,
+        streaming: bool = True,
         features: Optional[Dict[str, Any]] = None, **kwargs
     ):
     if features:
@@ -22,56 +27,163 @@ def load_dataset_from_files_or_hf(
     dataset = load_dataset(
         path=type_or_huggingface_path,
         data_files=filepaths,
-        field=field,
         split=split,
+        streaming=streaming,
         features=features,
         **kwargs
     )
+
     return dataset
 
+def apply_prompt_template_to_example(
+        tokenizer: PreTrainedTokenizerBase,
+        prompt_template: str,
+        example: Dict[str, str],
+        feature_fields: List[str],
+        label_field: Optional[str] = None, 
+        apply_chat_template: bool = True,
+        training: bool = False, 
+        add_generation_prompt: bool = False
+    ):
+    format_items = {
+        f: example[f] for f in feature_fields
+    }
+    label = None
 
-def tokenize_dataset(dataset: Dataset, tokenizer: PreTrainedTokenizerBase, field: Optional[str] = None, 
+    if label_field:
+        label = example[label_field]
+
+    prompt = prompt_template.format(**format_items)
+    return_dict = {}
+
+    if not apply_chat_template:
+        return_dict["texts"] = prompt
+        if label:
+            return_dict["labels"] = label
+    else:
+        chat = [
+            {"role": "user", "content": prompt}
+        ]
+        if training and label:
+            chat.append(
+                {"role": "assistant", "content": label}
+            )
+        
+        return_dict["texts"] = tokenizer.apply_chat_template(
+            conversation=chat,
+            tokenize=False,
+            add_generation_prompt=add_generation_prompt
+        )
+        if label:
+            return_dict["labels"] = label
+
+    return return_dict
+
+def apply_prompt_template_to_dataset(
+        tokenizer: PreTrainedTokenizerBase,
+        prompt_template: str, 
+        dataset: IterableDataset,
+        feature_fields: List[str],
+        label_field: str,
+        apply_chat_template: bool = True,
+        training: bool = False, 
+        add_generation_prompt: bool = False
+    ):
+    return (
+        dataset.map(
+            lambda example: apply_prompt_template_to_example(
+                tokenizer=tokenizer,
+                prompt_template=prompt_template,
+                example=example,
+                feature_fields=feature_fields,
+                label_field=label_field,
+                apply_chat_template=apply_chat_template,
+                training=training,
+                add_generation_prompt=add_generation_prompt
+            )
+            .with_format(format=format)
+        )
+    )
+
+def tokenize_example(tokenizer: PreTrainedTokenizerBase, example: Dict[str, Any], **tokenizer_kwargs):
+    outputs = {}
+    for f in fields:
+        output = tokenizer(example[f], **tokenizer_kwargs)
+        if isinstance(output, Mapping):
+            outputs[f] = output["input_ids"]
+        else:
+            outputs[f] = output
+
+    for key in example.keys():
+        if key not in outputs:
+            outputs[key] = example[key]
+    return outputs
+
+
+def tokenize_dataset(dataset: Dataset, tokenizer: PreTrainedTokenizerBase, fields:  List[str] = None, 
                      return_tensors: str = "pt", tokenizer_kwargs: Dict[str, Any] = {}, 
                      dataset_kwargs: Dict[str, Any]= {}):
-    dataset = dataset.with_transform(
-        lambda example: tokenizer(text=example[field] if field else example, **tokenizer_kwargs, return_tensors=return_tensors), 
+    dataset = dataset.map(
+        lambda example: tokenize_example(
+            tokenizer=tokenizer,
+            example=example,
+            fields=fields,
+            return_tensors=return_tensors,
+            **tokenizer_kwargs
+        ), 
         **dataset_kwargs
     )
     return dataset
 
 
-def chunk_example(item, pad_token_id: int, field_name: str = None, chunk_size: Union[Optional[int], Tuple[int, int]] = 4) -> torch.Tensor:
-    if field_name:
-        example = item[field_name]
-    else:
-        example = item
-    assert len(example.shape) <= 2, f"Assumed input should be of maximum 2 dimensions, got input of shape {example.shape}"
-    if len(example.shape) == 2:
-        assert example.shape[0] == 1, "Assumed working on single example, not a batch"
-    example = example.reshape(1, -1)
+
+def _chunk_tensor(tensor: torch.Tensor, pad_token_id: int, chunk_size: Union[Optional[int], Tuple[int, int]] = 4):
+    assert isinstance(tensor, torch.Tensor), f"Invalid type for tensor object: {type(tensor)}"
+    assert len(tensor.shape) <= 2, f"Assumed input should be of maximum 2 dimensions, got input of shape {tensor.shape}"
+    if len(tensor.shape) == 2:
+        assert tensor.shape[0] == 1, "Assumed working on single tensor, not a batch"
+    tensor = tensor.reshape(1, -1)
 
     if isinstance(chunk_size, tuple):
         ## Variable chunking
         assert len(chunk_size) == 2
         chunk_size = torch.randint(low=chunk_size[0], high=chunk_size[1], size=(1,), generator=random_generator, dtype=torch.int32).item()
-    right_padding = chunk_size - (example.shape[-1] % chunk_size)
-    example = pad(input=example, pad=(0, right_padding), mode="constant", value=pad_token_id)
-    return example.reshape(1, -1, chunk_size)
+    right_padding = chunk_size - (tensor.shape[-1] % chunk_size)
+    tensor = pad(input=tensor, pad=(0, right_padding), mode="constant", value=pad_token_id)
+    return tensor.reshape(1, -1, chunk_size)
+
+def chunk_example(item: Dict[str, Any], pad_token_id: int, field_name: Union[str, List[str]], chunk_field_name: str = "input_ids", chunk_size: Union[Optional[int], Tuple[int, int]] = 4) -> torch.Tensor:
+    outputs = {}
+    if isinstance(field_name, str):
+        example = item[field_name]
+        if isinstance(example, Mapping):
+            example = example[chunk_field_name]
+        outputs[field_name] = _chunk_tensor(example, pad_token_id=pad_token_id, chunk_size=chunk_size)
+    else:
+        for field in field_name:
+            outputs[field] = chunk_example(item, pad_token_id=pad_token_id, field_name=field, chunk_size=chunk_size).get(field)
+    return outputs
 
 
 def tokenize_and_chunk_dataset(dataset: Dataset, tokenizer: PreTrainedTokenizerBase, 
-                               tokenizer_field: Optional[str] = None, chunk_field: Optional[str] = "input_ids",
+                               tokenizer_fields: List[str], chunk_field_name: str = "input_ids",
                                chunk_size: Union[Optional[int], Tuple[int, int]] = 4, 
                                return_tensors: str = "pt", tokenizer_kwargs: Dict[str, Any] = {}, 
                                dataset_kwargs: Dict[str, Any]= {}):
     dataset = dataset.with_transform(
-        lambda item: {
-            "chunked_input_ids": chunk_example(
-                item=tokenizer(item[tokenizer_field] if tokenizer_field else item, return_tensors=return_tensors, **tokenizer_kwargs),
-                field_name=chunk_field,
+        lambda item: chunk_example(
+                item=tokenize_example(
+                    tokenizer=tokenizer,
+                    example=item,
+                    fields=tokenizer_fields,
+                    return_tensors=return_tensors,
+                    **tokenizer_kwargs
+                ),
+                field_name=tokenizer_fields,
+                chunk_field_name=chunk_field_name,
                 pad_token_id=tokenizer.pad_token_id,
                 chunk_size=chunk_size,
-            )},
+            ),
         **dataset_kwargs
     )
     return dataset
--- tests/datasets/test_dataset.py
+++ tests/datasets/test_dataset.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 
 import pytest
-import torch
+from torch.utils.data import DataLoader
 from transformers import AutoTokenizer
 
 from ouroboros.utils.data import (
     load_dataset_from_files_or_hf,
-    tokenize_and_chunk_dataset,
     tokenize_dataset,
+    apply_prompt_template_to_dataset
 )
 
 
@@ -15,10 +15,11 @@ def test_load_multiline_json_file():
     sample_train_data_file = Path(__file__).parent / "samples" / "sample_train.jsonl"
     train_data = load_dataset_from_files_or_hf(
         filepaths=str(sample_train_data_file),
-        split="train"
+        split="train",
+        streaming=False,
     )
     assert train_data.num_rows == 2
-    assert "text" in train_data.column_names
+    assert "text" in train_data.features
 
 
 @pytest.mark.xfail(raises=ValueError)
@@ -29,119 +30,46 @@ def test_data_load_failure():
         split="test"
     )
 
-
 def test_load_piqa():
     sample_data = load_dataset_from_files_or_hf(type_or_huggingface_path="ybisk/piqa", trust_remote_code=True)
-    assert sample_data["train"].num_rows > 0
+    assert sample_data["train"].dataset_size > 0
+    assert sample_data["test"].column_names == ["goal", "sol1", "sol2", "label"]
 
+def test_load_lambada():
+    sample_data = load_dataset_from_files_or_hf(type_or_huggingface_path="cimec/lambada", trust_remote_code=True)
+    assert sample_data["train"].dataset_size > 0
+    assert sample_data["test"].column_names == ["text", "domain"]
 
-def test_tokenization():
-    sample_train_data_file = Path(__file__).parent / "samples" / "sample_train.jsonl"
-    train_data = load_dataset_from_files_or_hf(
-        filepaths=str(sample_train_data_file),
-        split="train"
-    )
-    tokenizer = AutoTokenizer.from_pretrained("state-spaces/mamba-130m-hf")
-    tokenized_dataset = tokenize_dataset(
-        dataset=train_data, 
-        tokenizer=tokenizer,
-        field="text", 
-        tokenizer_kwargs={
-            "return_attention_mask": False,
-            "padding": True,
-            "truncation": True,
-            "max_length": 4096
-        }
+def test_apply_prompt_template():
+    sample_data = load_dataset_from_files_or_hf(type_or_huggingface_path="ybisk/piqa", trust_remote_code=True)
+    prompt_dataset = apply_prompt_template_to_dataset(
+        prompt_template="Your goal is as follows: {goal}. Pick the option corresponding the right solution.\n(1){sol1}\n(2){sol2}"
     )
 
-    for i, item in enumerate(tokenized_dataset):
-        assert "input_ids" in item
-        tokenized = tokenizer(train_data["text"][i], return_tensors="pt", return_attention_mask=False)["input_ids"]
-        ## since tokenization was done as batched 
-        ## we check if tokenization is correct upto where padding begins
-        assert torch.allclose(
-            tokenized[0], 
-            item["input_ids"]
-        )
+
+# def test_data_loader():
+#     sample_data = load_dataset_from_files_or_hf(type_or_huggingface_path="ybisk/piqa", trust_remote_code=True, split="train")
+#     tokenizer = AutoTokenizer.from_pretrained("state-spaces/mamba-130m-hf")
+
+#     fields=["goal", "sol1", "sol2"]
+
+#     tokenized_dataset = tokenize_dataset(
+#         sample_data, tokenizer, fields=fields,
+#         tokenizer_kwargs={
+#             "return_attention_mask": False
+#         }
+#     )
+
+#     data_collator = CustomDataCollator(
+#         tokenizer=tokenizer,
+#     )
+#     dataloader = DataLoader(
+#         dataset=tokenized_dataset,
+#         batch_size=16,
+#         collate_fn=data_collator,
+#     )
 
 
-def test_chunked_dataset():
-    sample_train_data_file = Path(__file__).parent / "samples" / "sample_train.jsonl"
-    train_data = load_dataset_from_files_or_hf(
-        filepaths=str(sample_train_data_file),
-        split="train"
-    )
-    tokenizer = AutoTokenizer.from_pretrained("state-spaces/mamba-130m-hf")
-    chunk_size = 4
-
-    tokenized_dataset = tokenize_dataset(
-        dataset=train_data, 
-        tokenizer=tokenizer,
-        field="text", 
-        tokenizer_kwargs={
-            "return_attention_mask": False,
-            "padding": True,
-            "truncation": True,
-            "max_length": 4096
-        }
-    )
-
-    chunked_dataset = tokenize_and_chunk_dataset(
-        dataset=train_data, 
-        tokenizer=tokenizer,
-        tokenizer_field="text", 
-        tokenizer_kwargs={
-            "return_attention_mask": False,
-            "padding": True,
-            "truncation": True,
-            "max_length": 4096
-        },
-        chunk_size=chunk_size,
-    )
-
-    for chunked_item, tokenized_item in zip(chunked_dataset, tokenized_dataset):
-        assert "chunked_input_ids" in chunked_item
-        item_elements = chunked_item["chunked_input_ids"].numel()
-        assert chunked_item["chunked_input_ids"].shape[-1] == chunk_size
-        assert chunk_size - (tokenized_item["input_ids"].shape[-1] % chunk_size) + tokenized_item["input_ids"].shape[-1] == item_elements
-
-
-def test_variable_chunked_dataset():
-    sample_train_data_file = Path(__file__).parent / "samples" / "sample_train.jsonl"
-    train_data = load_dataset_from_files_or_hf(
-        filepaths=str(sample_train_data_file),
-        split="train"
-    )
-    tokenizer = AutoTokenizer.from_pretrained("state-spaces/mamba-130m-hf")
-    chunk_size = (4, 64)
-
-    tokenized_dataset = tokenize_dataset(
-        dataset=train_data, 
-        tokenizer=tokenizer,
-        field="text", 
-        tokenizer_kwargs={
-            "return_attention_mask": False,
-            "padding": True,
-            "truncation": True,
-            "max_length": 4096
-        }
-    )
-
-    chunked_dataset = tokenize_and_chunk_dataset(
-        dataset=train_data, 
-        tokenizer=tokenizer,
-        tokenizer_field="text", 
-        tokenizer_kwargs={
-            "return_attention_mask": False,
-            "padding": True,
-            "truncation": True,
-            "max_length": 4096
-        },
-        chunk_size=chunk_size,
-    )
-
-    for chunked_item, tokenized_item in zip(chunked_dataset, tokenized_dataset):
-        assert "chunked_input_ids" in chunked_item
-        item_elements = chunked_item["chunked_input_ids"].numel()
-        item_chunk_size = chunked_item["chunked_input_ids"].shape[-1]
-        assert item_chunk_size - (tokenized_item["input_ids"].shape[-1] % item_chunk_size) + tokenized_item["input_ids"].shape[-1] == item_elements
+#     for i, batch in enumerate(dataloader):
+#         inputs = batch["input_ids"]
+#         labels = batch["labels"]
